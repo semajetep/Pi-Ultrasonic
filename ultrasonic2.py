8064c398d078d9aca9d166b471f6f689dbcdb9d2
Pete <pmawle@ocvc.ac.uk>
2013-06-16 00:43:22 +0200

    Create ultrasonic2.py
    UltraSonic test 2 this time a using a loop to track the data over time. Accuracy is not brilliant, takes 2 readings and returns the accuracy.

---
+++ ultrasonic2.py
@@ -0,0 +1,78 @@
+#
+# ultrasonic_2.py
+#
+
+# -----------------------
+# Import required Python libraries
+# -----------------------
+import time
+import RPi.GPIO as GPIO
+
+# -----------------------
+# Define some functions
+# -----------------------
+
+def measure():
+  # This function measures a distance
+  GPIO.output(GPIO_TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(GPIO_TRIGGER, False)
+  start = time.time()
+
+  while GPIO.input(GPIO_ECHO)==0:
+    start = time.time()
+
+  while GPIO.input(GPIO_ECHO)==1:
+    stop = time.time()
+
+  elapsed = stop-start
+  distance = (elapsed * 34300)/2
+
+  return distance
+
+def measure_average():
+  # This function takes 3 measurements and
+  # returns the average.
+  distance1=measure()
+  time.sleep(0.1)
+  distance2=measure()
+  time.sleep(0.1)
+  distance3=measure()
+  distance = distance1 + distance2 + distance3
+  distance = distance / 3
+  return distance
+
+# -----------------------
+# Main Script
+# -----------------------
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_TRIGGER = 23
+GPIO_ECHO    = 24
+
+print "Ultrasonic Measurement Loop Test"
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGGER, False)
+
+
+try:
+
+  while True:
+
+    distance = measure_average()
+    print "Distance : %.1f" % distance
+    time.sleep(1)
+
+except KeyboardInterrupt:
+  # User pressed CTRL-C
+  # Reset GPIO settings
+  GPIO.cleanup()
